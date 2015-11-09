10e9fd2327560184c826e10a281ba20a1ee513c7
TriHTM <trihtm123@gmail.com>
2015-11-09 10:53:54 +0700

    Update raw sql

---
+++ UPDATE2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sanpham_chitiet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tieude` varchar(255) DEFAULT NULL,
+  `giaban` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
